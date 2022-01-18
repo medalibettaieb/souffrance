a444d99a62aff4f341f395439bf5b9e04a321ac3
Mohamed Ali Bettaieb <mohamedali.bettaieb@sofrecom.com>
2022-01-18 15:04:46 +0100

    first

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (id, name) VALUES (1, 'USA');
+INSERT INTO product (id, name) VALUES (2, 'France');
+INSERT INTO product (id, name) VALUES (3, 'Brazil');
+INSERT INTO product (id, name) VALUES (4, 'Italy');
+INSERT INTO product (id, name) VALUES (5, 'Canada');
